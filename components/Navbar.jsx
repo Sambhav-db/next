ca90b2fe400e551cf29bac93c8ff784f05ce44e9
sambhav <db.sambhav@gmail.com>
2026-03-09 13:49:05 +0530

    layout , css , pages , app router

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link"; 
+
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <h2 className="logo">DevBlog Pro</h2>
+
+      <div className="links">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/blog">Blog</Link>
+      </div>
+    </nav>
+  );
+}
